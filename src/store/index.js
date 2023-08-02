ea1682aaf80c021164dbaa6b56cd8558a69a99a0
Ali Tayfour <ali.bassam.tayfour@gmail.com>
2023-08-02 14:51:36 +0300

    Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: "",
+    isAuthenticated: localStorage.getItem("Token") || false,
+    Authentication: localStorage.getItem("Token") || null
   },
   getters: {
+    isAuthenticated(state) {
+      return state.isAuthenticated;
+    },
+    Authentication(state) {
+      return state.Authentication;
+    }
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem("Token")) {
+        state.token = localStorage.getItem("Token");
+        state.isAuthenticated = true;
+      } else {
+        state.token = "";
+        state.isAuthenticated = false;
+      }
+    },
+    setToken(state, token) {
+      state.token = token;
+      state.Authentication = token;
+      state.isAuthenticated = true;
+    },
+    removeToken(state) {
+      state.token = "";
+      state.isAuthenticated = false;
+    }
   },
   actions: {
+
   },
-  modules: {
-  }
+  modules: {}
 })
